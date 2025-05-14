0509d2d1d786004685d6302517bc760861711e15
Matheus Victor <matheusvictor.dev@gmail.com>
2025-05-14 10:54:42 -0300

    feat: adiciona questao-03

---
+++ modulo-01-algoritmos/listas/lista-01/questao-03.js
@@ -0,0 +1,21 @@
+const prompt = require("prompt-sync")();
+
+const entrada = prompt("Digite uma nota: ");
+
+if (typeof parseInt(entrada) !== "number" || isNaN(entrada)) {
+  console.log("Entrada invalida!");
+} else {
+  nota = parseInt(entrada);
+  
+  if (nota >= 0 && nota <= 10){
+    if (nota < 5) {
+      console.log("Reprovado");
+    } else if (nota < 7) {
+      console.log("Recuperacao");
+    } else {
+      console.log("Aprovado");
+    }
+  } else {
+    console.log("Nota invalida!");
+  }
+}

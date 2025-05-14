258f665bdbeb4206332ce52ffdfe2ed591cc91eb
Matheus Victor <matheusvictor.dev@gmail.com>
2025-05-14 10:57:33 -0300

    fix: adiciona descricao da questao

--- modulo-01-algoritmos/listas/lista-01/questao-02.js
+++ modulo-01-algoritmos/listas/lista-01/questao-02.js
@@ -1,3 +1,7 @@
+// Crie um programa que classifica a idade de uma pessoa em categorias (criança,
+// adolescente, adulto, idoso) com base no valor fornecido, utilizando uma estrutura de
+// controle if-else.
+
 const prompt = require("prompt-sync")();
 
 const entrada = prompt("Digite uma idade: ");
--- modulo-01-algoritmos/listas/lista-01/questao-03.js
+++ modulo-01-algoritmos/listas/lista-01/questao-03.js
@@ -1,3 +1,6 @@
+// Implemente um programa que recebe uma nota de 0 a 10 e classifica como
+// "Aprovado", "Recuperação", ou "Reprovado" utilizando if-else if.
+
 const prompt = require("prompt-sync")();
 
 const entrada = prompt("Digite uma nota: ");
